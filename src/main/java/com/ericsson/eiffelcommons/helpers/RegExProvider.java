932ea48a940dc120f3bcd16fedca3104018d6db3
Anders Breid <anders.breid@ericsson.com>
2019-06-19 11:10:06 +0200

    Forgot to add files in previous commit

--- src/main/java/com/ericsson/eiffelcommons/helpers/RegExProvider.java
+++ src/main/java/com/ericsson/eiffelcommons/helpers/RegExProvider.java
@@ -1,9 +1,9 @@
-package com.ericsson.eiffelcommons.utils;
+package com.ericsson.eiffelcommons.helpers;
 
 public final class RegExProvider {
 
-  public static final String SUBSCRIPTION_NAME = "(\\W)";
-    public static final String NOTIFICATION_META = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";;
+    public static final String SUBSCRIPTION_NAME = "(\\W)";
+    public static final String NOTIFICATION_META = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 
     private RegExProvider() {
         throw new AssertionError();
