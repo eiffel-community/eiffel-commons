c54c53ee963ac2d12ee0d34313a34e06e43f29ab
Anders Breid <anders.breid@ericsson.com>
2019-07-04 08:47:55 +0200

    fixed after comment

--- src/main/java/com/ericsson/eiffelcommons/subscriptionobject/RestPostSubscriptionObject.java
+++ src/main/java/com/ericsson/eiffelcommons/subscriptionobject/RestPostSubscriptionObject.java
@@ -22,35 +22,40 @@ public class RestPostSubscriptionObject extends SubscriptionObject {
      */
     @Deprecated
     public void setBasicAuth(String username, String password) {
-        setAuthenticationTypeUsernameAndPassword("BASIC_AUTH", username, password);
-    }
-
-    /**
-     * Sets the field authenticationType together with the username and password.
-     * @param authenticationType
-     * @param username
-     * @param password
-     */
-    public void setAuthenticationTypeUsernameAndPassword(String authenticationType, String username, String password) {
-        setAuthenticationType(authenticationType);
-        setUsernameAndPassword(username, password);
+        setAuthenticationType("BASIC_AUTH");
+        setUsername(username);
+        setPassword(password);
     }
 
     /**
      * Sets the field authenticationType to given value
      * @param authenticationType
+     * @return RestPostSubscriptionObject
      */
-    public void setAuthenticationType(String authenticationType) {
+    public RestPostSubscriptionObject setAuthenticationType(String authenticationType) {
         subscriptionJson.put("authenticationType", authenticationType);
+        return this;
     }
 
     /**
-     * Sets the fields Username and Password in a subscription.
+     * Sets the field username in a subscription.
      * @param username
      * @param password
+     * @return RestPostSubscriptionObject
      */
-    public void setUsernameAndPassword(String username, String password) {
+    public RestPostSubscriptionObject setUsername(String username) {
         subscriptionJson.put("userName", username);
+        return this;
+    }
+
+    /**
+     * Sets the field password in a subscription.
+     * @param username
+     * @param password
+     * @return RestPostSubscriptionObject
+     */
+    public RestPostSubscriptionObject setPassword(String password) {
         subscriptionJson.put("password", password);
+        return this;
     }
 }
